b6bf954a9121aaaa1970ecc673930b1a5f35e92d
Naliat <tailanoliveira584@gmail.com>
2025-02-24 06:47:01 -0300

    Alterações Tailan

--- models/remedio.py
+++ models/remedio.py
@@ -1,6 +1,6 @@
 from bson import ObjectId  # Importe ObjectId do módulo bson
 from odmantic import Model, Field
-from datetime import datetime
+from datetime import datetime, timezone
 
 class Remedio(Model):
     nome: str
@@ -8,7 +8,7 @@ class Remedio(Model):
     preco: float = Field(gt=0)
     validade: datetime
     fornecedor_id: ObjectId  # Use ObjectId em vez de str
-    criado_em: datetime = Field(default_factory=datetime.utcnow)
-    atualizado_em: datetime = Field(default_factory=datetime.utcnow)
+    criado_em: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    atualizado_em: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
     model_config = {"collection": "remedios"}
